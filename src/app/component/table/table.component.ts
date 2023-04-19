f6d6e9a98f2c5e51c0bc00a09e965969c56b9d58
Luis D. Maco <luisdmaco@outlook.com>
2023-04-18 21:11:20 -0500

    quick fix methods table

--- src/app/component/table/table.component.ts
+++ src/app/component/table/table.component.ts
@@ -44,4 +44,16 @@ export class TableComponent implements OnInit{
       )
     }
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if(this.dataSource.paginator){
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  getRow(row: any){
+    this.router.navigateByUrl(`/detail/${row.position}`);
+  }
 }
