d9b120a5e55031de34ae8cd5dc81c299fbcf5fb2
Luis D. Maco <luisdmaco@outlook.com>
2023-04-18 20:20:01 -0500

    commit modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TableComponent } from './component/table/table.component';
+import { DetailComponent } from './component/detail/detail.component';
+
+const routes: Routes = [
+  {path:'home', component: TableComponent},
+  {path: 'detail/:id', component: DetailComponent },
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,3 @@
-export const environment = {};
+export const environment = {
+    baseUrl: 'https://pokeapi.co/api/v2/pokemon/1';
+};
